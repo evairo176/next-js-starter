fc372afe5e728826654cd616389ca2d2e44bc291
Dicki Prasetya <dickiprasetya176@gmail.com>
2025-06-06 20:44:58 +0700

    feat: update

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,3 +29,7 @@ export default async function middleware(req: NextRequest) {
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
