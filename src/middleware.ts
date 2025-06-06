cc162926822b00a0233cc7d6a29f37bdf5111057
Dicki Prasetya <dickiprasetya176@gmail.com>
2025-06-06 20:31:56 +0700

    feat: update 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 const protectedRoutes = ["/home", "/sessions"];
@@ -17,8 +18,8 @@ export default async function middleware(req: NextRequest) {
   const isProtectedRoute = protectedRoutes.includes(path);
   const isPublicRoute = publicRoutes.includes(path);
 
-  const accessToken = req.cookies.get("accessToken")?.value;
-  console.log({ accessToken });
+  const accessToken = cookies().get("accessToken");
+
   if (isProtectedRoute && !accessToken) {
     return NextResponse.redirect(new URL("/", req.nextUrl));
   }
