5639b9506c7a4d6626f2e50c16eca1ef5281ef3b
Dicki Prasetya <dickiprasetya176@gmail.com>
2025-06-06 22:02:23 +0700

    feat: update 101

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://nodejs-backend-starter.vercel.app/api/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
